c1cc620aaa076628e084b19ba14e601c57f6f18b
vijayasvj <65366176+vijayasvj@users.noreply.github.com>
2023-03-17 17:12:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
 import streamlit as st
 import requests
 from streamlit_lottie import st_lottie
+import requests
+from multiprocessing import Process
+import time
 
 body = st.container()
 
-
-
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-lottie = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_mwnl7iyc.json")
+placeholder = st.empty()
 
 with body:
-    st.title('Temperature and Humidity values')
-    st.write('For ROOM-1')
-    st.write('These are all dummyt as of now')
-    st.write('###')
-    #st.write('##')
-    st_lottie(lottie, height=250, key="coding")
-
+    st.title("Take Care of Your Mushrooms")
     left_column, right_column = st.columns(2)
-    with body:
-        st.markdown("Temperature Sensor")
-        st.write(f'<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2064592/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>',unsafe_allow_html=True,)
-        
-    with body:
-        st.markdown("Humidity Sensor")
-        st.write(f'<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2064592/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>',unsafe_allow_html=True,)
+    with left_column:
+        st.write(f'<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2069682/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>',unsafe_allow_html=True,)
+    with right_column:
+        while True:
+            with placeholder.container():
+                response = requests.get('https://api.thingspeak.com/channels/2069682/feeds.json?api_key=HEXPBNQ6XQK0QXCA&results=2')
+                prev = float(response.json()["feeds"][0]["field3"])
+                recent = float(response.json()["feeds"][1]["field3"])
+                delta = recent - prev
+                st.metric("Soil Moisture level", recent, delta=delta, delta_color="normal", help=None, label_visibility="visible")
+                time.sleep(1)
+            placeholder.empty()
